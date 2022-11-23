99cc36b0eb8d382a8bffb8dd2d650826a6fd6482
Rokon <71934517+rokon007@users.noreply.github.com>
2022-11-24 01:03:38 +0600

    prpfolio

--- example_parser.php
+++ example_parser.php
@@ -3,9 +3,9 @@ if( isset($_POST['n']) && isset($_POST['e']) && isset($_POST['m']) ){
 	$n = $_POST['n']; // HINT: use preg_replace() to filter the data
 	$e = $_POST['e'];
 	$m = nl2br($_POST['m']);
-	$to = "you@domain.com";	
+	$to = "rezaulkrm85@gmail.com";	
 	$from = $e;
-	$subject = 'Contact Form Message';
+	$subject = $_POST['s'];
 	$message = '<b>Name:</b> '.$n.' <br><b>Email:</b> '.$e.' <p>'.$m.'</p>';
 	$headers = "From: $from\n";
 	$headers .= "MIME-Version: 1.0\n";
